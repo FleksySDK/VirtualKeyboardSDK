2a038f8b193215a61379da68e12cf2d6cb90569c
Pierre Terroitin <pierre.terroitin@thingthing.co>
2022-04-22 13:18:46 +0200

    flutter - ios keyboard

--- integration/keyboard-flutter/ios/Runner/AppDelegate.swift
+++ integration/keyboard-flutter/ios/Runner/AppDelegate.swift
@@ -1,34 +1,53 @@
+import FleksySDK
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
         let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
         let channel = FlutterMethodChannel(name: "flutter.native/helper",
-                                                   binaryMessenger: controller.binaryMessenger)
-      channel.setMethodCallHandler({
-           (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-          
-          print(call.method)
-           
-         
-      
-      })
-      
-      
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-    
-    
-    func isImeEnabled() -> Bool {
-        return false
+                                           binaryMessenger: controller.binaryMessenger)
+        channel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            
+            
+            
+            print(call.method)
+            switch(call.method) {
+            case "isImeEnabled": result(self.isImeEnabled)
+            case "isImeSelected": result(self.isImeSelected())
+            case "enableIme": result(self.enableIme())
+            default: print("")
+            }
+            
+            
+        })
+        
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
+    
+    private func isImeSelected() -> Bool {
+        return true
+    }
+    
+    private var isImeEnabled: Bool {
+        return FleksyExtensionSetupStatus.isAddedToSettingsKeyboardExtension(withBundleId: "co.thingthing.integration.flutter")
+    }
+    
+    private func enableIme() -> Void {
+        let url = URL(string: UIApplication.openSettingsURLString)!
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+    }
     
 }
