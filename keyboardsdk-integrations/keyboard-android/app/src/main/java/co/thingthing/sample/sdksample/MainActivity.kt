4f4e93949768a601016d85bc3794698e58a40ba7
Aayush Gupta <aayush.gupta@thingthing.co>
2023-01-15 22:43:56 +0530

    keyboard-android: Add dependency & reformat project using ktlint
    Also fix dummy test package's import and package name
    
    Signed-off-by: Aayush Gupta <aayush.gupta@thingthing.co>

--- keyboardsdk-integrations/keyboard-android/app/src/main/java/co/thingthing/sample/sdksample/MainActivity.kt
+++ keyboardsdk-integrations/keyboard-android/app/src/main/java/co/thingthing/sample/sdksample/MainActivity.kt
@@ -12,56 +12,55 @@ import co.thingthing.sample.sdksample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-	private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		binding = ActivityMainBinding.inflate(layoutInflater)
-		setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-		binding.apply {
-			imeEnabled.setOnClickListener { showInputMethodSettings() }
-			imeCurrent.setOnClickListener { showInputMethodPicker() }
-			tryEditText.setOnEditorActionListener { v, _, _ -> v.text = ""; true }
-		}
-	}
+        binding.apply {
+            imeEnabled.setOnClickListener { showInputMethodSettings() }
+            imeCurrent.setOnClickListener { showInputMethodPicker() }
+            tryEditText.setOnEditorActionListener { v, _, _ -> v.text = ""; true }
+        }
+    }
 
-	override fun onWindowFocusChanged(hasFocus: Boolean) {
-		super.onWindowFocusChanged(hasFocus)
-		updateStatus()
-	}
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        updateStatus()
+    }
 
-	private fun showInputMethodSettings() {
-		startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
-	}
+    private fun showInputMethodSettings() {
+        startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+    }
 
-	private fun showInputMethodPicker() {
-		inputMethodManager?.showInputMethodPicker()
-	}
+    private fun showInputMethodPicker() {
+        inputMethodManager?.showInputMethodPicker()
+    }
 
-	private fun updateStatus() {
-		setStatus(binding.imeEnabled, isImeEnabled)
-		setStatus(binding.imeCurrent, isImeCurrent)
-	}
+    private fun updateStatus() {
+        setStatus(binding.imeEnabled, isImeEnabled)
+        setStatus(binding.imeCurrent, isImeCurrent)
+    }
 
-	private fun setStatus(view: TextView, status: Boolean) {
-		view.text = status.toString()
-		view.setTextColor(if (status) Color.GREEN else Color.RED)
-	}
+    private fun setStatus(view: TextView, status: Boolean) {
+        view.text = status.toString()
+        view.setTextColor(if (status) Color.GREEN else Color.RED)
+    }
 
-	private val isImeEnabled
-		get() = enabledInputMethods.any { it.packageName == packageName }
+    private val isImeEnabled
+        get() = enabledInputMethods.any { it.packageName == packageName }
 
-	private val isImeCurrent
-		get() = defaultInputMethod.startsWith("$packageName/")
+    private val isImeCurrent
+        get() = defaultInputMethod.startsWith("$packageName/")
 
-	private val inputMethodManager
-		get() = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+    private val inputMethodManager
+        get() = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
 
-	private val enabledInputMethods
-		get() = inputMethodManager?.enabledInputMethodList ?: emptyList()
+    private val enabledInputMethods
+        get() = inputMethodManager?.enabledInputMethodList ?: emptyList()
 
-	private val defaultInputMethod
-		get() = Settings.Secure.getString(contentResolver, Settings.Secure.DEFAULT_INPUT_METHOD)
-
+    private val defaultInputMethod
+        get() = Settings.Secure.getString(contentResolver, Settings.Secure.DEFAULT_INPUT_METHOD)
 }
