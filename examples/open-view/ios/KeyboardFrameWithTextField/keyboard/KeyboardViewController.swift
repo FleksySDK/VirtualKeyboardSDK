7b6b74b618fb64671105bda56222ff042cf35732
Antonio J Pallares <antonio.pallares@thingthing.co>
2023-04-18 09:31:37 +0200

    🔥 Simplify `KeyboardViewController`s in iOS open-view examples

--- examples/open-view/ios/KeyboardFrameWithTextField/keyboard/KeyboardViewController.swift
+++ examples/open-view/ios/KeyboardFrameWithTextField/keyboard/KeyboardViewController.swift
@@ -9,36 +9,15 @@
 import UIKit
 import FleksyKeyboardSDK
 
-// MARK: - KeyboardViewController
-
 class KeyboardViewController: FKKeyboardViewController {
-    
-    // MARK: View Controller life cycle
-    
-    /// - Important: Every time the keyboard appears it calls in this order: ``viewDidLoad`` -> ``viewWillAppear`` -> ``viewDidAppear``.
-    /// Keyboard extensions don't reuse the view, which means that, in every appearance, we recreate what's inside ``viewDidLoad``.
-    /// This behaviour is different from the normal iOS ViewController.
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+        
     override func createConfiguration() -> KeyboardConfiguration {
-                
-        // KEYBOARD STYLE
-        let style = StyleConfiguration()
-      
         // License Configuration
         let licenseConfig = LicenseConfiguration(licenseKey: "<your-license-key>", licenseSecret: "<your-license-secret>")
         
         // KEYBOARD CONFIGURATION --
-        let config = KeyboardConfiguration( style: style,
-                                           specialKeys: nil,
-                                            apps: AppsConfiguration(keyboardApps: [KeyboardFrameViewCustom.init()], showAppsInCarousel: false), /// This is a new parameter. We share the View that might be invoked.
+        let config = KeyboardConfiguration(apps: AppsConfiguration(keyboardApps: [KeyboardFrameViewCustom.init()], showAppsInCarousel: false), /// This is a new parameter. We share the View that might be invoked.
                                            license: licenseConfig)
-        
-        if #available(iOSApplicationExtension 11.0, *) {
-            config.needsInputMethodSwitch = self.needsInputModeSwitchKey
-        }
         return config
     }
     
