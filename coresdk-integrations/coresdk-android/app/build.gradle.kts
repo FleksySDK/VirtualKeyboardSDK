cd11320f8a2a87c38efcf5fc5a67baace5c12a67
Iago <iago.torrado@thingthing.co>
2023-06-05 10:19:18 +0200

    Update Android Predictive SDK to version 1.0.5

--- coresdk-integrations/coresdk-android/app/build.gradle.kts
+++ coresdk-integrations/coresdk-android/app/build.gradle.kts
@@ -6,13 +6,12 @@ plugins {
 }
 
 android {
-    compileSdk = 32
-    buildToolsVersion = "30.0.3"
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "co.thingthing.fleksy.lib.app"
         minSdk = 24
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0.0"
 
@@ -64,25 +63,26 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    namespace = "co.thingthing.fleksy.lib.app"
 }
 
 dependencies {
-    implementation("co.thingthing.fleksylib:fleksylib-release:1.0.1")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("co.thingthing.fleksylib:fleksylib-release:1.0.5")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     //Hilt
-    implementation("com.google.dagger:hilt-android:2.43.2")
-    kapt("com.google.dagger:hilt-android-compiler:2.43.2")
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.44.2")
 
     //navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- coresdk-integrations/coresdk-android/build.gradle.kts
+++ coresdk-integrations/coresdk-android/build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.2.2")
+        classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
     }
 }
