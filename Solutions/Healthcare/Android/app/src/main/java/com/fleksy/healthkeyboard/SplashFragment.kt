fdea302ec63c42c3c9a17dc2687a6d3eb33c082a
Xevi Farrarons <xevi.farrarons@fleksy.com>
2023-11-22 12:47:19 +0100

    Add Solution for Healthcare

---
+++ Solutions/Healthcare/Android/app/src/main/java/com/fleksy/healthkeyboard/SplashFragment.kt
@@ -0,0 +1,32 @@
+package com.fleksy.healthkeyboard
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.fleksy.healthkeyboard.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSplashBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.nav_splash_to_home)
+        }, 2000)
+    }
+}
