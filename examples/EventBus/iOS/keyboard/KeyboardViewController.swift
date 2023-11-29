0f09346696d62214500bf062710e7478ecf44824
Xevi Farrarons <xevi.farrarons@fleksy.com>
2023-11-29 11:59:49 +0100

    Add Example for EventBus

--- examples/EventBus/iOS/keyboard/KeyboardViewController.swift
+++ examples/EventBus/iOS/keyboard/KeyboardViewController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 import FleksyKeyboardSDK
-import GiphyApp
 import Combine
+
 
 // MARK: - KeyboardViewController
 
@@ -32,20 +32,51 @@ class KeyboardViewController: FKKeyboardViewController {
         
         // TypingConfiguration which includes punctuationSymbols
         let typing = TypingConfiguration()
-        
+
         let licenseConfig = LicenseConfiguration(licenseKey: "your-license-key", licenseSecret: "your-license-secret")
-        
-        let appsConfig = AppsConfiguration(keyboardApps: [GiphyApp(apiKey: "your-Giphy-api-key")],
-                                           showAppsInCarousel: true)
         
         //
         // Create the configuration for the keyboard
         //
         let config = KeyboardConfiguration( style: style,
                                            typing: typing,
-                                           apps: appsConfig,
                                            license: licenseConfig)
         
         return config
+    }
+
+    
+    //
+    // EventBus Usage
+    //
+    
+    // Subscribe to the events that we want
+    //
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToEvents()
+    }
+    
+    // UnSubscribe to the events that we want
+    //
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        observations.removeAll()
+    }
+    
+    private var observations: Set<AnyCancellable> = []
+    
+    private func subscribeToEvents(){
+        eventBus.activity.sink { activityEvent in
+            print("Activity event: \(activityEvent)")
+            
+            // Important! All eventBus events come from a background thread, if you want to make UI changes, use the main thread.
+            //
+        }
+        .store(in: &observations)
+        
+        eventBus.configuration.sink { configurationEvent in
+            print("Configuration event: \(configurationEvent)")
+        }.store(in: &observations)
     }
 }
