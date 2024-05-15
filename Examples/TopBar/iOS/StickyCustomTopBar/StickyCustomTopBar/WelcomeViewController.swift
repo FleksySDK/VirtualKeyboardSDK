b15284b9804497975c1dc61e377bce94d7758392
Antonio J Pallares <antonio.pallares@thingthing.co>
2024-05-15 12:46:20 +0200

    feat: add new iOS sample for sticky custom top bar
    This sample exists to illustrate this discussion https://github.com/FleksySDK/VirtualKeyboardSDK/discussions/139

---
+++ Examples/TopBar/iOS/StickyCustomTopBar/StickyCustomTopBar/WelcomeViewController.swift
@@ -0,0 +1,159 @@
+//
+//  WelcomeViewController.swift
+//  StickyCustomTopBar
+//
+//  Copyright © 2024 Thingthing,Ltd. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for details
+//
+
+import UIKit
+import FleksyKeyboardSDK
+
+class WelcomeViewController: UITableViewController {
+    
+    private static let rightDetailCellReuseIdentifier = "RightDetailSettingsCellId"
+    
+    private static var fleksyKeyboardAdded: Bool {
+        return FleksyExtensionSetupStatus.isAddedToSettingsKeyboardExtension(withBundleId: Constants.App.keyboardExtensionBundleId)
+    }
+    
+    private struct Item {
+        let titleKey: String
+        let detailKey: String?
+        let type: ItemType
+        
+        init(titleKey: String, subtitleKey: String? = nil, type: ItemType) {
+            self.titleKey = titleKey
+            self.detailKey = subtitleKey
+            self.type = type
+        }
+    }
+    
+    enum ItemAction {
+        case openURL(url: URL)
+    }
+    
+    private enum ItemType {
+        case link(path: String)
+        case info
+        
+        var action: ItemAction? {
+            switch self {
+            case .link(let path):
+                guard let url = URL(string: path) else {
+                    return nil
+                }
+                return .openURL(url: url)
+            case .info:
+                return nil
+            }
+        }
+        
+        var cellIdentifier: String {
+            switch self {
+            case .info:
+                return WelcomeViewController.rightDetailCellReuseIdentifier
+            default:
+                return WelcomeViewController.rightDetailCellReuseIdentifier
+            }
+        }
+    }
+    
+    private struct Section {
+        let titleKey: String
+        let items: [Item]
+        
+        init(titleKey: String, items: [Item?]) {
+            self.titleKey = titleKey
+            self.items = items.compactMap { $0 }
+        }
+    }
+    
+    private lazy var sections: [Section] = []
+    
+    private static func getSections() -> [Section] {
+        return [
+            Section(titleKey: "Information", items: [
+                Item(titleKey: "App version",
+                     subtitleKey: Constants.App.versionAndBuild,
+                     type: .info),
+                Item(titleKey: "FleksyKeyboardSDK version",
+                     subtitleKey: Constants.App.keyboardSDKVersionAndBuild,
+                     type: .info),
+                Item(titleKey: "Keyboard status",
+                     subtitleKey: fleksyKeyboardAdded ? "Installed" : "Not installed",
+                     type: fleksyKeyboardAdded ? .info : .link(path: UIApplication.openSettingsURLString))
+            ])
+        ]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = NSLocalizedString("Fleksy Development Examples", comment: "")
+        NotificationCenter.default.addObserver(self, selector: #selector(appDidBecomeActive(_:)), name: UIApplication.didBecomeActiveNotification, object: nil)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reloadData()
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let item = sender as? Item {
+            segue.destination.title = NSLocalizedString(item.titleKey, comment: "")
+        }
+    }
+    
+    @objc func appDidBecomeActive(_ notification: Notification) {
+        reloadData()
+    }
+    
+    // MARK: - Private functions
+    
+    private func reloadData() {
+        sections = Self.getSections()
+        tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewControllerDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sections[section].items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = sections[indexPath.section].items[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: item.type.cellIdentifier)!
+        cell.textLabel?.text = NSLocalizedString(item.titleKey, comment: "")
+        cell.detailTextLabel?.text = item.detailKey.map {
+            NSLocalizedString($0, comment: "")
+        }
+        cell.accessoryType = item.type.action == nil ? .none : .disclosureIndicator
+        return cell
+    }
+    
+    // MARK: - UITableViewControllerDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = sections[indexPath.section].items[indexPath.row]
+        switch item.type.action {
+        case .none:
+            return
+        case .openURL(url: let url):
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        NSLocalizedString(sections[section].titleKey, comment: "")
+    }
+}
---
+++ Examples/TopBar/iOS/StickyCustomTopBar/keyboard/KeyboardFrameViewCustom.swift
@@ -0,0 +1,106 @@
+//
+//  KeyboardFrameViewCustom.swift
+//  keyboard
+//
+//  Copyright © 2024 Thingthing,Ltd. All rights reserved.
+//  Licensed under the MIT license. See LICENSE file in the project root for details
+//
+
+import UIKit
+import FleksyAppsCore
+
+///
+/// Example on how to add a View on top of the keyboard
+/// This can be cusomized to your own needs
+///
+class KeyboardFrameViewCustom : KeyboardApp, AppTextFieldDelegate {
+    
+    static let appId = "someId.example" // Static ID used to call this View from the keyboardViewController
+    
+    var appId: String { Self.appId }
+    var configuration: AppConfiguration?
+    var listener: AppListener?
+    var exampleView: UIView?
+    var label: UILabel?
+        
+    func initialize(listener: AppListener, configuration: AppConfiguration) {
+        self.listener = listener
+        self.configuration = configuration
+    }
+    
+    func dispose() {
+        listener = nil
+        configuration = nil
+    }
+    
+    /// The view mode for the FleksyApp when opened from the carousel.
+    ///
+    /// Override this property if your FleksyApp needs a different initial view mode.
+    open var defaultViewMode: KeyboardAppViewMode {
+        .frame(barMode: .default, height: .default)
+    }
+    
+    ///
+    /// From the keyboardViewController you can decide when to call this method,
+    /// which will show the view that you send here.
+    ///
+    func open(viewMode: KeyboardAppViewMode, theme: AppTheme) -> UIView? {
+        if exampleView == nil || label == nil {
+            createYourOwnView()
+        }
+        return exampleView
+    }
+    
+    
+    /// This is gonna be called automatically by the system when you close the View.
+    func close() {
+        // Free all references created from the open()
+        exampleView = nil
+        label = nil
+    }
+    
+    func onThemeChanged(_ theme: AppTheme) {
+        // Change the color of the theme if you want
+    }
+    
+    func appIcon() -> UIImage? {
+        // This image is used only:
+        // 1. In the apps carousel
+        // 2. When using TopBarMode.appTextField.
+        // Therefore, it can be nil for
+        return nil
+    }
+    
+    //
+    // Create your own view to add on top of the keyboard
+    //
+    func createYourOwnView() {
+        
+        let exampleView = UIView()
+        
+        // Configure the example View
+        exampleView.backgroundColor = .darkGray
+                
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = "Frame View that appears as soon as the keyboard opens"
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        exampleView.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: exampleView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: exampleView.centerYAnchor),
+            label.widthAnchor.constraint(equalToConstant: 300),
+            label.heightAnchor.constraint(lessThanOrEqualTo: exampleView.heightAnchor),
+        ])
+        
+        self.exampleView = exampleView
+        self.label = label
+    }
+    
+    func onCloseAction() {
+        // Only applies when using TopBarMode.appTextField
+    }
+}
